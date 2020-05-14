808c7214a39936ddd60984fbf2706689406cca17
Marcel Haupt <subcortexx@gmail.com>
2020-05-14 14:03:17 +0200

    Common function file added

---
+++ dist/common.functions.js
@@ -0,0 +1,51 @@
+var common = {
+	pickEnergySource: function(creep) 
+    {
+	    //try to find half full containers
+	    var c = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return ((structure.structureType == STRUCTURE_CONTAINER) &&
+                    structure.store.getUsedCapacity(RESOURCE_ENERGY)/structure.store.getCapacity(RESOURCE_ENERGY) > 0.3) || 
+                    ((structure.structureType == STRUCTURE_CONTAINER) && structure.pos.findInRange(FIND_SOURCES, 2).length == 0 && 
+                    structure.store.getUsedCapacity(RESOURCE_ENERGY) > 0);
+            }
+        });
+        if (c)
+        {
+	        creep.memory.source = c.id;
+	    } else {
+		    var s = creep.pos.findClosestByPath(FIND_SOURCES);
+		    if (s) {
+		    	creep.memory.source = s.id;
+		    }
+	    }
+    }, 
+    
+    
+	goGetEnergyFromSource: function(creep)
+	{
+		var source = Game.getObjectById(creep.memory.source);
+        if (!source) { delete creep.memory.source; return; }
+        
+        if (source instanceof Source) {
+	        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ff0000'}});
+            }
+        }
+        else
+        {
+	        if(creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ff0000'}});
+            }
+            
+            //source empty - search other one
+            if (source.store.getUsedCapacity(RESOURCE_ENERGY) == 0)
+            {
+	            delete creep.memory.source;
+            }
+        }
+	}
+}
+
+
+module.exports = common;
