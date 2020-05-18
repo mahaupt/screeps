9f600675d14833370ee267c3ecb91b63b4af361f
Marcel Haupt <subcortexx@gmail.com>
2020-05-18 16:32:34 +0200

    Wall positions predefined

--- src/module.autobuilder.js
+++ src/module.autobuilder.js
@@ -130,6 +130,9 @@ var moduleAutobuilder = {
                 }
 			}
 	    }
+        
+        //todo: building walls
+        
     },
     
     
@@ -318,6 +321,15 @@ var moduleAutobuilder = {
         positions[STRUCTURE_ROAD] = [
             {x:-1, y:0}, {x:-1, y:-1}, 
             {x:0, y:-1}
+        ];
+        
+        positions[STRUCTURE_WALL] = [
+            {x:6, y:7}, {x:5, y:7}, {x:4, y:7}, {x:3, y:7}, {x:2, y:7}, {x:1, y:7},
+            {x:6, y:6}, {x:6, y:5}, {x:6, y:4}, {x:6, y:3}, {x:6, y:2}
+        ];
+        
+        positions[STRUCTURE_RAMPART] = [
+            {x:0, y:7}, {x:6, y:1}, {x:6, y:0}
         ];
         
         
