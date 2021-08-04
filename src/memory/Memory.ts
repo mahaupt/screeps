f24f98f038678d6d23a1f2aa8231eb60c866283b
cbacon93 <subcortexx@gmail.com>
2021-08-04 23:14:35 +0200

    Add first class

---
+++ src/memory/Memory.ts
@@ -0,0 +1,13 @@
+
+
+
+export class Mem {
+  static clean() : void {
+    // Automatically delete memory of missing creeps
+    for (const name in Memory.creeps) {
+      if (!(name in Game.creeps)) {
+        delete Memory.creeps[name];
+      }
+    }
+  }
+}
