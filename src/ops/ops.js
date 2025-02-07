4da435143a211133fb113231d9d4cdee8afcd17b
mahaupt <subcortexx@gmail.com>
2025-02-07 09:49:16 +0100

    Add prettier

--- src/ops/ops.js
+++ src/ops/ops.js
@@ -4,55 +4,57 @@ Ops List
 
 */
 
-const opsAttack = require('ops_ops.attack');
-const opsClaim = require('ops_ops.claim');
-const opsDefend = require('ops_ops.defend');
-const opsDrain = require('ops_ops.drain');
-const opsScout = require('ops_ops.scout');
-const opsHarvest = require('ops_ops.harvest');
-const opsScoutVicinity  = require('ops_ops.scout_vicinity');
-const opsRoomLifetime = require('ops_ops.room_lifetime');
+const opsAttack = require("ops_ops.attack");
+const opsClaim = require("ops_ops.claim");
+const opsDefend = require("ops_ops.defend");
+const opsDrain = require("ops_ops.drain");
+const opsScout = require("ops_ops.scout");
+const opsHarvest = require("ops_ops.harvest");
+const opsScoutVicinity = require("ops_ops.scout_vicinity");
+const opsRoomLifetime = require("ops_ops.room_lifetime");
 
 module.exports = {
-    run: function() {
+    run: function () {
         if (!Memory.ops) {
             Memory.ops = [];
         }
-        
+
         //remove finished
         _.remove(Memory.ops, (s) => s.finished == true);
-        
+
         //run
-        for(var i in Memory.ops) {
-            if (Memory.ops[i].type == 'attack') {
+        for (var i in Memory.ops) {
+            if (Memory.ops[i].type == "attack") {
                 opsAttack.run(Memory.ops[i]);
-            } else if (Memory.ops[i].type == 'claim') {
+            } else if (Memory.ops[i].type == "claim") {
                 opsClaim.run(Memory.ops[i]);
-            } else if (Memory.ops[i].type == 'defend') {
+            } else if (Memory.ops[i].type == "defend") {
                 opsDefend.run(Memory.ops[i]);
-            } else if (Memory.ops[i].type == 'drain') {
+            } else if (Memory.ops[i].type == "drain") {
                 opsDrain.run(Memory.ops[i]);
-            } else if (Memory.ops[i].type == 'scout') {
+            } else if (Memory.ops[i].type == "scout") {
                 opsScout.run(Memory.ops[i]);
-            } else if (Memory.ops[i].type == 'harvest') {
+            } else if (Memory.ops[i].type == "harvest") {
                 opsHarvest.run(Memory.ops[i]);
-            } else if (Memory.ops[i].type == 'scout_vicinity') {
+            } else if (Memory.ops[i].type == "scout_vicinity") {
                 opsScoutVicinity.run(Memory.ops[i]);
-            } else if (Memory.ops[i].type == 'room_lifetime') {
+            } else if (Memory.ops[i].type == "room_lifetime") {
                 opsRoomLifetime.run(Memory.ops[i]);
             }
         }
-    }, 
-    
-    
-    new: function(type, source, target, mem={}) 
-    {
-        Memory.ops.push({type: type, source: source, target: target, finished: false, mem: mem});
     },
-    
-    
-    checkSrcRoomAvbl: function(ops)
-    {
+
+    new: function (type, source, target, mem = {}) {
+        Memory.ops.push({
+            type: type,
+            source: source,
+            target: target,
+            finished: false,
+            mem: mem,
+        });
+    },
+
+    checkSrcRoomAvbl: function (ops) {
         // SOURCE ROOM NOT AVBL - ABORT
         if (!Game.rooms[ops.source]) {
             console.log("Ops." + ops.type + ": Source room lost. Aborting");
@@ -60,5 +62,5 @@ module.exports = {
             return true;
         }
         return false;
-    }
+    },
 };
